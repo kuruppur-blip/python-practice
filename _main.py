cab3b6c46d49eb6d434c7e7c215a25fe7153f465
kuruppur-blip <kuruppur@wsc.school.nz>
2026-05-12 15:07:31 +1200

    changed the codes ]

--- _main.py
+++ _main.py
@@ -1,15 +1,15 @@
-# print("hello")
-# print()
-# print("hello")
-# print('hello')
-# print("I'm coding")
-# print("I'm coding")
+print("hello")
+print()
+print("hello")
+print('hello')
+print("I'm coding")
+print("I'm coding")
 
 
-# print( 5 + 5)
-# print("5" + "5")
-# print(5.0 + 5.0)
- print("5" + 5)
+print( 5 + 5)
+print("5" + "5")
+print(5.0 + 5.0)
+print("5" + 5)
 print(5 + 5.0)
 
 name = "Rasini Kuruppu"
