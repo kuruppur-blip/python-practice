d49f4242500d39075e8d59608e646f1d8ef0fda7
kuruppur-blip <kuruppur@wsc.school.nz>
2026-05-12 14:48:00 +1200

    added more codes to the file.

---
+++ _main.py
@@ -0,0 +1,27 @@
+# print("hello")
+# print()
+# print("hello")
+# print('hello')
+# print("I'm coding")
+# print("I'm coding")
+
+
+# print( 5 + 5)
+# print("5" + "5")
+# print(5.0 + 5.0)
+ print("5" + 5)
+print(5 + 5.0)
+
+name = "Rasini Kuruppu"
+age = "15"
+print(name + " is " + age )
+name = input("What's your name?")
+
+
+name = "Helen"
+age = 15 
+print(name + " is " + age)
+
+name = "Helen"
+age = 15 
+print(name, "is", age)
