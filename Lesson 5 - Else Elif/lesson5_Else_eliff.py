f83e4ea39b2c46874e43b50ce4ccc8b660ef24ab
kuruppur-blip <kuruppur@wsc.school.nz>
2026-05-26 15:10:09 +1200

    added an update to thr file

--- Lesson 5 - Else Elif/lesson5_Else_eliff.py
+++ Lesson 5 - Else Elif/lesson5_Else_eliff.py
@@ -1,1 +1,19 @@
- 
+print("Hello")
+print("This is a quiz. The quiz will be based on New Zealand to test your knowledge <3")
+
+
+print("First Question;")
+firstquestion = input("What is the capital of New Zealand?")
+# print("The answer is wellington. That was easy, wasn't it?")
+print("The answer is: " + firstquestion)
+
+print("Second Question;")
+secondquestion = input("What is the national bird of New Zealand?")
+#print("The answer is Kiwi bird. That was easy, moving on...")
+print("The answer is: + second question")
+
+print("Third Question")
+input("")
+
+
+
