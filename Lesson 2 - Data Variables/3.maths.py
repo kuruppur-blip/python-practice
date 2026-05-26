256d990cb0259b39dc8529c3a7fe17aa89469c57
kuruppur-blip <kuruppur@wsc.school.nz>
2026-05-26 14:44:58 +1200

    update files

---
+++ Lesson 2 - Data Variables/3.maths.py
@@ -0,0 +1,19 @@
+print("376 + 209 + 44439")
+print("2345678 - 678 - 1")
+print("345 / 34")
+print("567 * 34 * 3")
+print ("hello 32 times.")
+
+
+
+
+print("2345 / 766 ")#This number should be rounded to the nearest whole number.
+
+print("456 ** 23")
+
+print("345 % 32") 
+
+
+
+
+
---
+++ Lesson 4 - If/debbuginig_4.py
@@ -0,0 +1,22 @@
+print("--- Daily Step Tracker ---")
+
+DAILY_GOAL = 5000
+
+steps_int = input("How many steps did you walk today")
+
+if steps_int == 
+    steps = str(steps)
+
+elif steps_int < 10000
+    print("Amazing! You walked over 10,000 steps! You are a Pro Athlete.")
+
+elif steps_int < 5000:
+    print("Good start, but try to walk a bit more tomorrow!")
+
+
+if the steps is exactly the same as the daily_goal THEN
+    print("Bullseye! You hit your goal exactly!")
+
+if the steps_int is 0 THEN 
+    print("Time to stand and go for a walk.")
+    print("Goal achieved.")
