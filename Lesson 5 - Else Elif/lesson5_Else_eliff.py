d54b5cc334ff980882e81e3200a9e9d200bf71e1
kuruppur-blip <kuruppur@wsc.school.nz>
2026-05-27 09:54:57 +1200

    added the question and the final touches.

--- Lesson 5 - Else Elif/lesson5_Else_eliff.py
+++ Lesson 5 - Else Elif/lesson5_Else_eliff.py
@@ -12,9 +12,48 @@ secondquestion = input("What is the national symbol of New Zealand?")
 #print("The answer is Kiwi. That was easy, moving on...")
 print("The answer is:" + secondquestion)
 
-print("Third Question")
+print("Third Question;")
 thirdquestion = input("What is the name of New Zealand's indigenous people?")
 #print("The answer is Maori. Next question.")
-print("The anwer is:" + thirdquestion)
+print("The answer is:" + thirdquestion)
+
+print("Fourth Question;")
+forthquestion = input("What are the two main islands of New Zealand called?")
+#print("The answer is North and South Islands. Next question.")
+print("The answer is:" + forthquestion)
+
+print("Fifth question;")
+fifthquestion = input("Which famous movie trillogy was filmed extensively in New Zealand?")
+#print("The answer is The lord of the rings. Next question.")
+print("The answer is:" + fifthquestion)
+
+print("Sixth question;")
+sixthquestion = input("What are the Maori name for New Zealand?")
+#print("The answer is Aotearoa. Next question.")
+print("The answer is:" + sixthquestion)
+
+print("Seventh Question;")
+seventhquestion = input("Which city is known as the CITY OF SAILS?")
+#print("The answer is Auckland. Next question.")
+print("The answer is" + seventhquestion)
+
+print("Eighth Question;")
+eighthquestion = input("What major tectonic feature runs through New Zealnd?")
+#print("The answer is The Alpine Fault.")
+print("The answer is:" + eighthquestion)
+
+print("Ninth Question")
+ninthquestion = input("WHich New Zealand bird is the world's only alpine parrot?")
+#print("The answer is kea. Next question.")
+print("The answer is:" + ninthquestion)
+
+print("Tenth Question")
+tenthquestion = input("Who was the first person to reach the summit of MOunt EVerest, and is one of New Zealand's most famous figures?")
+#print("The answer is EDmund Hillary.")
+print("The answer is:" + tenthquestion)
+
+print("WELL DONE!")
+
+
 
 
