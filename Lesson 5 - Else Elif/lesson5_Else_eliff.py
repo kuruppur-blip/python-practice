418cc385af211bc4d1e522fae3ebd021eb224445
kuruppur-blip <kuruppur@wsc.school.nz>
2026-05-28 09:10:41 +1200

    corrected some things in the quiz.

--- Lesson 5 - Else Elif/lesson5_Else_eliff.py
+++ Lesson 5 - Else Elif/lesson5_Else_eliff.py
@@ -1,11 +1,13 @@
+#This is a quiz to the a person's Knowledge. 
+
 print("Hello")
-print("This is a quiz. The quiz will be based on New Zealand to test your knowledge (●'◡'●)")
+print("This is a quiz. The quiz will be based on New Zealand to test your knowledge. (●'◡'●)")
 
 
 print("First Question;")
 firstquestion = input("What is the capital of New Zealand?")
 # print("The answer is wellington. That was easy, wasn't it?")
-print("The answer is: " + firstquestion)
+print("The answer is:" + firstquestion)
 
 print("Second Question;")
 secondquestion = input("What is the national symbol of New Zealand?")
@@ -35,24 +37,26 @@ print("The answer is:" + sixthquestion)
 print("Seventh Question;")
 seventhquestion = input("Which city is known as the CITY OF SAILS?")
 #print("The answer is Auckland. Next question.")
-print("The answer is" + seventhquestion)
+print("The answer is:" + seventhquestion)
 
 print("Eighth Question;")
 eighthquestion = input("What major tectonic feature runs through New Zealnd?")
 #print("The answer is The Alpine Fault.")
 print("The answer is:" + eighthquestion)
 
-print("Ninth Question")
+print("Ninth Question;")
 ninthquestion = input("WHich New Zealand bird is the world's only alpine parrot?")
 #print("The answer is kea. Next question.")
 print("The answer is:" + ninthquestion)
 
-print("Tenth Question")
+print("Tenth Question;")
 tenthquestion = input("Who was the first person to reach the summit of MOunt EVerest, and is one of New Zealand's most famous figures?")
-#print("The answer is EDmund Hillary.")
+#print("The answer is Edmund Hillary.")
 print("The answer is:" + tenthquestion)
 
-print("WELL DONE!")
+
+
+print("WELL DONE! (❁´◡`❁)")
 
 
 
