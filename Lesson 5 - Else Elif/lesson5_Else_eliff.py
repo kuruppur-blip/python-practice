c6ba897afdc40ece1e34b09ad47b1f2be0261ca7
kuruppur-blip <kuruppur@wsc.school.nz>
2026-06-02 15:05:27 +1200

    finished it

--- Lesson 5 - Else Elif/lesson5_Else_eliff.py
+++ Lesson 5 - Else Elif/lesson5_Else_eliff.py
@@ -20,66 +20,120 @@ secondquestion = input("What is the national symbol of New Zealand?").strip()
 if secondquestion == "answer":
 #print("The answer is Kiwi. That was easy, moving on...") 
     print("Well done.")
-    print("The answer is:" + secondquestion)
+    print("The answer is: " + secondquestion)
 elif secondquestion == "Kiwi":
     print("Well done")
-    print("The answer is:" + secondquestion)
+    print("The answer is: " + secondquestion)
 else:
     print("Uh oh! That's not right.")
-    print("The answer is:" + secondquestion)
+    print("The answer is: " + secondquestion)
 
 print("Third Question;")
-thirdquestion = input("What is the name of New Zealand's indigenous people?")
-if thirdquestion == "Maori":
+thirdquestion = input("What is the name of New Zealand's indigenous people?").strip()
+if thirdquestion == "answer":
 #print("The answer is Maori. Next question.")
     print("Well done.")
-    print("The answer is:" + thirdquestion)
+    print("The answer is: " + thirdquestion)
 elif thirdquestion == "Maori": 
     print("Well done.")
-    print("The answer is" + thirdquestion)
+    print("The answer is: " + thirdquestion)
 else:
-    print
-
+    print("Uh oh! That's not right.")
+    print("The answer is: " + thirdquestion)
 
 print("Fourth Question;")
-forthquestion = input("What are the two main islands of New Zealand called?")
+fourthquestion = input("What are the two main islands of New Zealand called?").strip()
+if fourthquestion == "answer":
 #print("The answer is North and South Islands. Next question.")
-print("The answer is:" + forthquestion)
+    print("Well done.")
+    print("The answer is: " + fourthquestion)
+elif fourthquestion == "North and South Islands":
+    print("Well done.")
+    print("The answer is: " + fourthquestion)
+else:
+    print("Uh oh! That's not right.")
+    print("The answer is: " + fourthquestion)
 
 print("Fifth question;")
-fifthquestion = input("Which famous movie trillogy was filmed extensively in New Zealand?")
+fifthquestion = input("Which famous movie trillogy was filmed extensively in New Zealand?").strip()
+if fifthquestion == "answer": 
 #print("The answer is The lord of the rings. Next question.")
-print("The answer is:" + fifthquestion)
+    print("Well done.")
+    print("The answer is: " + fifthquestion)
+elif fifthquestion ==  "The lord of the rings": 
+    print("Well done.")
+    print("The answer is: " + fifthquestion)
+else:
+    print("Uh oh! That's not right.")
+    print("The answer is: " + fifthquestion)
 
 print("Sixth question;")
-sixthquestion = input("What are the Maori name for New Zealand?")
+sixthquestion = input("What are the Maori name for New Zealand?").strip()
+if sixthquestion == "answer":
 #print("The answer is Aotearoa. Next question.")
-print("The answer is:" + sixthquestion)
+    print("Well done.")
+    print("The answer is: " + sixthquestion)
+elif sixthquestion == "Aotearoa":
+    print("Well done.")
+    print("The answer is: " + sixthquestion)
+else:
+    print("Uh oh! That's not right.")
+    print("The answer is: " + sixthquestion)
 
 print("Seventh Question;")
 seventhquestion = input("Which city is known as the CITY OF SAILS?")
+if seventhquestion == "answer":
 #print("The answer is Auckland. Next question.")
-print("The answer is:" + seventhquestion)
+    print("Well done.")
+    print("The answer is: " + seventhquestion)
+elif seventhquestion == "Auckland":
+    print("Well done.")
+    print("The answer is: " + seventhquestion)
+else: 
+    print("Uh oh! That's not right.")
+    print("The answer is: " + seventhquestion)
 
 print("Eighth Question;")
 eighthquestion = input("What major tectonic feature runs through New Zealnd?")
+if eighthquestion == "answer":
 #print("The answer is The Alpine Fault.")
-print("The answer is:" + eighthquestion)
+    print("Well done.")
+    print("The answer is: " + eighthquestion)
+elif eighthquestion == "Alpine Fault":
+    print("Well done.")
+    print("The answer is: " + eighthquestion)
+else:
+    print("Uh oh! That's not right.")
+    print("The answer is: " + eighthquestion)
 
 print("Ninth Question;")
-ninthquestion = input("WHich New Zealand bird is the world's only alpine parrot?")
+ninthquestion = input("Which New Zealand bird is the world's only alpine parrot?")
+if ninthquestion == "answer":
 #print("The answer is kea. Next question.")
-print("The answer is:" + ninthquestion)
+    print("Well done.")
+    print("The answer is: " + ninthquestion)
+elif ninthquestion == "kea":
+    print("Well done.")
+    print("The answer is: " + ninthquestion)
+else:
+    print("Uh oh! That's not right.")
+    print("The answer is: " + ninthquestion)
 
 print("Tenth Question;")
 tenthquestion = input("Who was the first person to reach the summit of MOunt EVerest, and is one of New Zealand's most famous figures?")
+if tenthquestion == "answer":
 #print("The answer is Edmund Hillary.")
-print("The answer is:" + tenthquestion)
-
+    print("Well done.")
+    print("The answer is: " + tenthquestion)
+elif tenthquestion == "Edmund Hillary":
+    print("Well done.")
+    print("The answer is: " + tenthquestion)
+else:
+    print("Uh oh! That's not right.")
+    print("The answer is: " + tenthquestion)
 
 
 print("WELL DONE! (❁´◡`❁)")
 print("Thank you for playing")
 print("Bye bye. ╰(*°▽°*)╯")
-
 
